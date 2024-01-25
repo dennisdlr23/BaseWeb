8ed5ed0ea6a39275116850f61f7c070fdb2dbd89
Josue Argueta <josue_emilio94@hotmail.com>
2024-01-25 09:56:19 -0600

    Initial
    Login, usuarios, roles, objetos

---
+++ src/app/pages/items/warehouse/models/warehouse.ts
@@ -0,0 +1,10 @@
+export class WareHouseModel {
+    id: number;
+    name: string;
+    createByName: string;
+    createBy: number;
+    createDate: Date;
+    updateBy: number;
+    updateDate: Date;
+    active: true;
+}
